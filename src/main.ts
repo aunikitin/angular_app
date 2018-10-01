bc38c9bdac843e25d5932b0ab9bd2ef4a4566493
aunikitin <sanya_nikiti@mail.ru>
2018-10-01 23:21:37 +0500

    'test'

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import 'core-js/es6';
+// для поддержки Reflect Api
+import 'core-js/es7/reflect';
+// zone используется angular
+import 'zone.js/dist/zone';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
+
+const platform = platformBrowserDynamic();
+platform.bootstrapModule(AppModule);
